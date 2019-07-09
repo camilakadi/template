0096452cd699a9d8978762d816ca88456694ef1e
Camila Kadi <camilakadi@hotmail.com>
2019-07-09 19:37:10 -0300

    subindo

--- js/template.js
+++ js/template.js
@@ -1,34 +1,66 @@
-
-includeHTML();
-
-function includeHTML() {
-	var z, i, elmnt, file, xhttp;
-	/* Loop through a collection of all HTML elements: */
-	z = document.getElementsByTagName("*");
-	for (i = 0; i < z.length; i++) {
-		elmnt = z[i];
-		/*search for elements with a certain atrribute:*/
-		file = elmnt.getAttribute("w3-include-html");
-		if (file) {
-			/* Make an HTTP request using the attribute value as the file name: */
-			xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    /* Remove the attribute, and call this function once more: */
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            };
-			xhttp.open("GET", file, true);
-			xhttp.send();
-			/* Exit the function: */
-			return;
-		}
-	}
-}
+inicializarPagina();
 
 $().ready(function () {
-    $("#formTelefone").validate();
+    $("#formTelefone").validate({
+        rules: {
+            adata: {
+                required: true
+            }
+        },
+        messages: {
+            adata: {
+                required: "Campo obrigatório"
+            },
+            aabandonosChamada: {
+                required: "Campo obrigatório"
+            },
+            atempoMedioEspera: {
+                required: "Campo obrigatório"
+            },
+            atempoMedioAtendimento: {
+                required: "Campo obrigatório"
+            },
+            anivelServico: {
+                required: "Campo obrigatório"
+            }
+        }
+    });
 });
+
+function inicializarPagina() {
+    mostrarValoresTelefone();
+}
+
+function pegarValoresFormTelefone() {
+    let formularioTelefone = {
+        data: document.getElementById("adata").value,
+        abandonosChamada: document.getElementById("aabandonosChamada").value,
+        tempoMedioEspera: document.getElementById("atempoMedioEspera").value,
+        tempoMedioAtendimento: document.getElementById("atempoMedioAtendimento").value,
+        nivelServico: document.getElementById("anivelServico").value
+    };
+
+    armazenarLocalStorage("formularioTelefone", formularioTelefone);
+}
+
+function armazenarLocalStorage(chave, objeto) {
+    window.localStorage.setItem(chave, JSON.stringify(objeto));
+}
+
+function pegarLocalStorage(chave) {
+    return JSON.parse(window.localStorage.getItem(chave));
+}
+
+function mostrarValoresTelefone() {
+    let formularioTelefone = pegarLocalStorage("formularioTelefone");
+    console.log(formularioTelefone);
+    console.log(formularioTelefone.data);
+
+
+    document.getElementById("dataTelefoneTexto").innerHTML = formularioTelefone.data;
+
+}
+
+function limparFormularioTelefone() {
+    document.getElementById("formTelefone").reset();
+}
